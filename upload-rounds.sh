0b69405b1d43fb075aba431e855596da79fbaaa5
Nil Geisweiller <ngeiswei@gmail.com>
2014-10-02 00:47:17 +0200

    Add more metadata

--- upload-rounds.sh
+++ upload-rounds.sh
@@ -22,15 +22,43 @@ PROG_PATH=$(readlink -f "$0")
 PROG_DIR=$(dirname "$PROG_PATH")
 . "$PROG_DIR/common.sh"
 
+#############
+# Functions #
+#############
+
+# Given a round return the date of that round
+get_date() {
+    local ROUND=$1
+    while read row; do
+        
+        row_round=$(get_value "$row" round)
+
+        # If the round doesn't match, skip that entry. Or break if it's
+        # passed the target round
+        if [[ $row_round -gt $ROUND ]]; then
+            fatalError "Round $ROUND cannot be found in file $METADATA"
+        elif [[ $row_round -lt $ROUND ]]; then
+            continue
+        fi
+
+        # Get the rest of the metadata
+        echo $(get_value "$row" date)
+    done < <(tail -n+2 $METADATA)
+}
+
 ########
 # Main #
 ########
 
 for rnd in $@; do
     pad_rnd=$(pad $rnd 3)
-    identifier=SDCompo_Round_${pad_rnd}_UPLOAD_TEST_FIX_METADATA
+    identifier=SDCompo_Round_${pad_rnd}_UPLOAD_TEST_REFIX_METADATA
     infoEcho "Upload rendered files to $identifier"
     ia upload $identifier "$RENDERS_DIR"/round${rnd}/*.flac \
         --metadata="mediatype:audio" \
-        --metadata="collection:opensource_audio"
+        --metadata="collection:opensource_audio" \
+        --metadata="date:$(get_date $rnd)" \
+        --metadata="licenseurl:http://creativecommons.org/licenses/by-nc-nd/3.0/" \
+        --metadata="description:Fill this with something useful"
+    # TODO add subject tags
 done
